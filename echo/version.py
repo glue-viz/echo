8346740e667b6cb9221c836dd80184c9e4e7cf76
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2023-04-03 15:26:29 -0400

    Fix import logic for older versions of Python

--- echo/version.py
+++ echo/version.py
@@ -1,5 +1,10 @@
-import importlib.metadata
+import sys
+
+if sys.version_info >= (3, 9):
+    import importlib.metadata as importlib_metadata
+else:
+    import importlib_metadata
 
 __all__ = ['__version__']
 
-__version__ = importlib.metadata.version('echo')
+__version__ = importlib_metadata.version('echo')
